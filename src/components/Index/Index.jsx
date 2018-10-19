f48a9b4a638352a60324f62a1cf54a9217afd3ff
Udo Nkwocha <writeudo@gmail.com>
2018-10-19 17:30:47 +0100

    added ant

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Index.scss';
 import Board from 'components/Board/Board';
+import Ant from 'components/Ant/Ant';
 
 const Index = () => (
 	<div className="index">
-		<Board width={805} height={450} rows={25} cols={60} />
+		<div className="container">
+			<Board width={805} height={450} rows={25} cols={60} />
+			<Ant />
+		</div>
 	</div>
 );
 
