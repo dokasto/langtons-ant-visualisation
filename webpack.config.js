67c098f94f709b70ec8e9e58d7e76377b556df54
Udo <writeudo@gmail.com>
2018-10-20 20:11:46 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const ENVIRONMENT = process.env.NODE_ENV;
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 
@@ -47,8 +46,7 @@ const config = {
 			}
 		}),
 		new webpack.DefinePlugin({ 'global.GENTLY': false }),
-		new webpack.NoEmitOnErrorsPlugin(),
-		new ExtractTextPlugin('style.css')
+		new webpack.NoEmitOnErrorsPlugin()
 	],
 
 	resolve: {
@@ -63,22 +61,23 @@ const config = {
 		rules: [
 			{
 				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					use: [{ loader: 'css-loader', options: { minimize: true, importLoaders: 1 } }, postCSSLoader, { loader: 'sass-loader' }]
-				})
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						{
-							loader: 'css-loader',
-							options: { minimize: true, importLoaders: 1 }
+				use: [
+					{ loader: 'style-loader' },
+					{
+						loader: 'css-loader',
+						options: { minimize: true, importLoaders: 1 }
+					},
+					postCSSLoader,
+					{
+						loader: 'sass-loader',
+						options: {
+							includePaths: [path.resolve(__dirname, './node_modules/compass-mixins/lib')],
+							sourceMap: true
 						}
-					]
-				})
+					}
+				]
 			},
+			{ test: /\.css$/, exclude: /node_modules/, loader: ['css-loader'] },
 			{ test: /\.jsx?$/, exclude: /node_modules/, loader: ['babel-loader'] },
 			{ test: /\.json$/, exclude: /node_modules/, loader: 'json' },
 			{ test: /\.(png|jpg|gif)$/, loader: 'file-loader?limit=100000' },
@@ -95,27 +94,6 @@ if (ENVIRONMENT === 'development') {
 	config.entry.bundle.unshift('webpack-hot-middleware/client');
 	config.entry.bundle.unshift('webpack/hot/dev-server');
 	config.plugins.push(new webpack.HotModuleReplacementPlugin());
-
-	config.module.rules[0] = {
-		test: /\.scss$/,
-		use: [
-			{ loader: 'style-loader' },
-			{
-				loader: 'css-loader',
-				options: { minimize: true, importLoaders: 1 }
-			},
-			postCSSLoader,
-			{
-				loader: 'sass-loader',
-				options: {
-					includePaths: [path.resolve(__dirname, './node_modules/compass-mixins/lib')],
-					sourceMap: true
-				}
-			}
-		]
-	};
-
-	config.module.rules[1] = { test: /\.css$/, exclude: /node_modules/, loader: ['css-loader'] };
 } else {
 	/**
 	 * PRODUCTION!
